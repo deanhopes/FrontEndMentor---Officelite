f7e57180849c9475eec50e5266a72f1d96d76b7a
Dean <dean@blueocto.co.uk>
2021-02-16 09:43:00 +0000

    added sass

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// https://andrejgajdos.com/custom-select-dropdown/
 // Toggles Open on custom select wrapper
+// selects document
 document
+  //selects the custom-select-wrapper class
   .querySelector('.custom-select-wrapper')
+  //adds an eventlistener for click onto custom-select-wrapper class
   .addEventListener('click', () => {
+    // on click it runs the below function
     document.querySelector('.custom-select').classList.toggle('open');
+    // the function selects the custom-select class and toggles open
   });
 
+// for each option of the custom-option class
 for (const option of document.querySelectorAll('.custom-option')) {
+  // add the click event listener
   option.addEventListener('click', () => {
-    console.log(this);
+    // check if document class does NOT contain 'selected'
     if (!document.classList.contains('selected')) {
       document.parentNode
         .querySelector('.custom-option.selected')
